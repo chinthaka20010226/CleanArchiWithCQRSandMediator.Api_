a30065a594546c0340a4cd741257bdad1ca44a47
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-13 12:16:45 +0530

    Services Configuration in Application Layer

--- Application/ConfigureServices.cs
+++ Application/ConfigureServices.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application
 {
     public static class ConfigureServices
-    {                                                   
+    {               
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(ctg =>
+            {
+                ctg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            return services;
+        }
     }
 }

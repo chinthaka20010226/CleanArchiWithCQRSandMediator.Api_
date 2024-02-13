4056b3322b87a39fafc396c963a4b33d51be0be0
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-02-13 12:40:19 +0530

    Infrastructure -> Microsoft.Extensions.Configuration.Abstraction, Project Reference -> Application, Service Configuration

---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddInfrestructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
